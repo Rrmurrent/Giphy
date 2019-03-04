41274f63e6138945693d0b9065bd6d1a578c11e9
Smarty Pants <amcurrent@hotmail.com>
2019-03-04 15:27:03 -0600

    uploaded buttons with appropriate ajax calls

--- assets/logic.js
+++ assets/logic.js
@@ -1,44 +1,16 @@
-alert("hi");
 
-// var mainDiv = document.getElementById('mainDiv');
+$('button').on('click',function () {
+    var x = $(this).data("search");
+    console.log(x);
 
-// var subjectarray= ["dog","cat","bird","penguin", "giraffe"]
-// Going to need a four loop
-// for (let index = 0; index < array.length; index++) {
-    // const element = array[index];
-// };
+var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +x+ "&api_key=8rWTYSGMgDTrwqndYxIEjk7QT2CSpo0D&limit=10"
+  console.log(queryURL);
 
-// var display = function () {
-//     mainDiv.empty(); 
-// };
-
-var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=8rWTYSGMgDTrwqndYxIEjk7QT2CSpo0D";
-
-$.ajax({
-  url: queryURL,
-  method: "GET"
+  $.ajax({url:queryURL,method:'Get'})
+  .done(function(response){
+    console.log(response);
+    for(var i=0; i<response.data.length;i++) {
+    $('#gifArea').prepend("<img src='"+response.data[i].images.downsized.url + "'>");
+    }
+  })
 })
-
-.then(function(response) {
-  console.log(response.data[0]);
-  $("#gallery").html("<img src='" + response.data[11].images.fixed_height.url + "'>");
-  
-
-
-
-var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=8rWTYSGMgDTrwqndYxIEjk7QT2CSpo0D&tag=dogs";
-
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-    var imageUrl = response.data.image_original_url;
-
-    var dogImage = $("<img>");
-
-    dogImage.attr("src",imageUrl);
-    dogImage.attr("alt", "dog image");
-
-         $("#gallery").prepend(dogImage);
-    });
-});
